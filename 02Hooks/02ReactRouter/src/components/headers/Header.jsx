d884dce446da4495aa7806093eed8cbe364ca534
prashantbhoite123 <pbhoite985@gmail.com>
2024-06-09 13:35:25 +0530

    fix isActive problem

--- 02Hooks/02ReactRouter/src/components/headers/Header.jsx
+++ 02Hooks/02ReactRouter/src/components/headers/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
               <li>
                 <NavLink
                   to="/"
-                  className={(isActive) =>
+                  className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 border-b  ${
                       isActive ? "text-orange-700" : "text-gray-700"
                     }  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -47,7 +47,7 @@ export default function Header() {
               <li>
                 <NavLink
                   to="/about"
-                  className={(isActive) =>
+                  className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 border-b ${
                       isActive ? "text-orange-700" : "text-gray-700"
                     }  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -59,7 +59,7 @@ export default function Header() {
               <li>
                 <NavLink
                   to="/contact"
-                  className={(isActive) =>
+                  className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 ${
                       isActive ? "text-orange-700" : "text-gray-700"
                     }  border-b  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -71,7 +71,7 @@ export default function Header() {
               <li>
                 <NavLink
                   to="/github"
-                  className={(isActive) =>
+                  className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 border-b ${
                       isActive ? "text-orange-700" : "text-gray-700"
                     }  border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
